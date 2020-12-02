13dbef63c4bf711af5ad2d17a2b317e1e7ea6feb
Double-X <lkllun@yahoo.com.hk>
2020-12-02 14:55:17 +0800

    Updated to v1.00b

--- DoubleX_RMMZ_Plugin_Query.js
+++ DoubleX_RMMZ_Plugin_Query.js
@@ -67,13 +67,6 @@
  *      9. https://www.patreon.com/posts/42482628
  *      10. https://www.makerdevs.com/plugin/doublex-rmmz-plugin-query
  *----------------------------------------------------------------------------
- *    # Instructions
- *      1. The default plugin parameters file name is
- *         DoubleX_RMMZ_Plugin_Query
- *         If you want to change that, you must edit the value of
- *         DoubleX_RMMZ.Plugin_Query.PLUGIN_NAME, which must be done
- *         via opening this plugin js file directly
- *----------------------------------------------------------------------------
  *    # Contributors
  *      Authors:
  *      1. DoubleX
@@ -87,6 +80,10 @@
  *      - None So Far
  *----------------------------------------------------------------------------
  *    # Changelog
+ *      { codebase: "1.1.0", plugin: "v1.00b" }(2020 Dec 2 GMT 0700):
+ *      1. You no longer have to edit the value of
+ *         DoubleX_RMMZ.Plugin_Query.PLUGIN_NAME when changing this plugin
+ *         file name
  *      { codebase: "1.0.2", plugin: "v1.00a" }(2020 Oct 6 GMT 1600):
  *      1. 1st version of this plugin finished
  *============================================================================*/
@@ -111,7 +108,7 @@
 /*:
  * @url https://www.patreon.com/doublex
  * @target MZ
- * @plugindesc Versions: { codebase: "1.0.2", plugin: "v1.00a" }
+ * @plugindesc Versions: { codebase: "1.1.0", plugin: "v1.00b" }
  * Lets you use plugin queries in conditional branch and control variables
  * @orderAfter DoubleX_RMMZ_Enhanced_Codebase
  * @orderAfter DoubleX RMMZ Enhanced Codebase
@@ -179,12 +176,23 @@
 // jshint esversion: 6
 
 var DoubleX_RMMZ = DoubleX_RMMZ || {}; // var must be used or game will crash
-// Separates the version numbers with the rest to make the former more clear
-DoubleX_RMMZ.Plugin_Query = {
-    PLUGIN_NAME: "DoubleX_RMMZ_Plugin_Query",
-    VERSIONS: { codebase: "1.0.2", plugin: "v1.00a" }
-}; // DoubleX_RMMZ.Plugin_Query
-//
+
+(() => {
+
+    "use strict";
+
+    const src = document.currentScript.src;
+    const name = src.split("/").slice(-1)[0].split(".")[0].replace(/%20/g, " ");
+    console.info(src, name);
+
+    // Separates the version numbers with the rest to make the former more clear
+    DoubleX_RMMZ.Plugin_Query = {
+        PLUGIN_NAME: "DoubleX_RMMZ_Plugin_Query",
+        VERSIONS: { codebase: "1.1.0", plugin: "v1.00b" }
+    }; // DoubleX_RMMZ.Plugin_Query
+    //
+
+})();
 
 (PQ => {
 
