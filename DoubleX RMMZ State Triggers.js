6553192e27f8e1162536a2ca8ea5d0172f6e5799
Double-X <lkllun@yahoo.com.hk>
2020-12-02 11:05:26 +0800

    Updated to v1.00b

--- DoubleX RMMZ State Triggers.js
+++ DoubleX RMMZ State Triggers.js
@@ -54,13 +54,6 @@
  *      9. https://www.patreon.com/posts/doublex-rmmz-40988080
  *      10. https://www.makerdevs.com/plugin/doublex-rmmz-state-triggers
  *----------------------------------------------------------------------------
- *    # Instructions
- *      1. The default plugin parameters file name is
- *         DoubleX RMMZ State Triggers
- *         If you want to change that, you must edit the value of
- *         DoubleX_RMMZ.State_Triggers.PLUGIN_NAME, which must be done via
- *         opening this plugin js file directly
- *----------------------------------------------------------------------------
  *    # Contributors
  *      Authors:
  *      1. DoubleX
@@ -74,13 +67,17 @@
  *      - None So Far
  *----------------------------------------------------------------------------
  *    # Changelog
+ *      { codebase: "1.1.0", plugin: "v1.00b" }(2020 Dec 2 GMT 0400):
+ *      1. You no longer have to edit the value of
+ *         DoubleX_RMMZ.State_Triggers.PLUGIN_NAME when changing this plugin
+ *         file name
  *      { codebase: "1.0.0", plugin: "v1.00a" }(2020 Aug 29 GMT 1300):
  *      1. 1st version of this plugin finished
  *============================================================================*/
 /*:
  * @url https://www.patreon.com/doublex
  * @target MZ
- * @plugindesc Versions: { codebase: "1.0.0", plugin: "v1.00a" }
+ * @plugindesc Versions: { codebase: "1.1.0", plugin: "v1.00b" }
  * Lets you run some codes set by your notetags on some important state timings
  * @orderAfter DoubleX_RMMZ_Enhanced_Codebase
  * @orderAfter DoubleX RMMZ Enhanced Codebase
@@ -401,12 +398,24 @@
 // jshint esversion: 6
 
 var DoubleX_RMMZ = DoubleX_RMMZ || {}; // var must be used or game will crash
-// Separates the version numbers with the rest to make the former more clear
-DoubleX_RMMZ.State_Triggers = {
-    PLUGIN_NAME: "DoubleX RMMZ State Triggers",
-    VERSIONS: { codebase: "1.0.0", plugin: "v1.00a" }
-}; // DoubleX_RMMZ.State_Triggers
-//
+
+(() => {
+
+    "use strict";
+
+    const src = document.currentScript.src;
+    const name = src.split("/").slice(-1)[0].split(".")[0].replace(/%20/g, " ");
+    console.info(src, name);
+
+    // Separates the version numbers with the rest to make the former more clear
+    DoubleX_RMMZ.State_Triggers = {
+        PLUGIN_NAME: "DoubleX RMMZ State Triggers",
+        VERSIONS: { codebase: "1.1.0", plugin: "v1.00b" }
+    }; // DoubleX_RMMZ.State_Triggers
+    //
+    //
+
+})();
 
 (ST => {
 
