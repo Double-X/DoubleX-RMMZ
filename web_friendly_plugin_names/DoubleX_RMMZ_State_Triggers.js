f5336c9e00f7dcbe07e1a593341a4cceca1a8022
Double-X <lkllun@yahoo.com.hk>
2020-12-02 14:26:22 +0800

    Update DoubleX_RMMZ_State_Triggers.js

--- web_friendly_plugin_names/DoubleX_RMMZ_State_Triggers.js
+++ web_friendly_plugin_names/DoubleX_RMMZ_State_Triggers.js
@@ -409,7 +409,7 @@ var DoubleX_RMMZ = DoubleX_RMMZ || {}; // var must be used or game will crash
 
     // Separates the version numbers with the rest to make the former more clear
     DoubleX_RMMZ.State_Triggers = {
-        PLUGIN_NAME: "DoubleX RMMZ State Triggers",
+        PLUGIN_NAME: name,
         VERSIONS: { codebase: "1.1.0", plugin: "v1.00b" }
     }; // DoubleX_RMMZ.State_Triggers
     //
