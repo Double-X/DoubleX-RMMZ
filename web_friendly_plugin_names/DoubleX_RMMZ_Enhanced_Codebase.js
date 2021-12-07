35fac0f8e75992b330c0c39055a964f5e9c8c28d
Double-X <lkllun@yahoo.com.hk>
2021-12-07 16:07:55 +0800

    Update to codebase v1.3.3

--- web_friendly_plugin_names/DoubleX_RMMZ_Enhanced_Codebase.js
+++ web_friendly_plugin_names/DoubleX_RMMZ_Enhanced_Codebase.js
@@ -68,7 +68,7 @@
  *         must be outdated if those version numbers are indeed different
  *      3. (Plugin developers only)The version numbers of this plugin are
  *         stored in DoubleX_RMMZ.Enhanced_Codebase.VERSIONS, which should be
- *         { codebase: "1.2.1", plugin: "v0.00a" }
+ *         { codebase: "1.3.3", plugin: "v0.00a" }
  *         If it's falsy, it means this plugin's not loaded at the moment of
  *         querying its version numbers
  *      4. (Plugin developers only)Please use the following search tag to
@@ -126,7 +126,7 @@
 /*:
  * @url https://www.patreon.com/doublex
  * @target MZ
- * @plugindesc Versions: { codebase: "1.2.1", plugin: "v0.00a" }
+ * @plugindesc Versions: { codebase: "1.3.3", plugin: "v0.00a" }
  * Fixes bugs, improves codebase quality, boosts performance and gives new APIs
  * @author DoubleX
  *
@@ -413,7 +413,7 @@ var DoubleX_RMMZ = DoubleX_RMMZ || {}; // var must be used or game will crash
     // Separates the version numbers with the rest to make the former more clear
     DoubleX_RMMZ.Enhanced_Codebase = {
         PLUGIN_NAME: name,
-        VERSIONS: { codebase: "1.2.1", plugin: "v0.00a" }
+        VERSIONS: { codebase: "1.3.3", plugin: "v0.00a" }
     }; // DoubleX_RMMZ.Enhanced_Codebase
     //
 
@@ -1569,6 +1569,7 @@ var DoubleX_RMMZ = DoubleX_RMMZ || {}; // var must be used or game will crash
         if (this._width === width && this._height === height) return;
         //
         [this._width, this._height] = [width, height];
+        this._app.renderer.resize(width, height);
         this._updateAllElements();
     }); // v0.00a - v0.00a
 
